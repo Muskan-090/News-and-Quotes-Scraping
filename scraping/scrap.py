4770aa36ca582d7cc0cdc257828cff2c9134bf8f
Muskan Sonkar <87746861+Muskan-090@users.noreply.github.com>
2022-08-16 12:38:47 +0530

    Update scrap.py

--- scraping/scrap.py
+++ scraping/scrap.py
@@ -1,3 +1,4 @@
+from urllib import request
 from numpy import result_type
 from  selenium import webdriver
 from django.http import HttpResponse
@@ -5,50 +6,53 @@ from time import sleep
 from bs4 import BeautifulSoup
 import requests
 
-def WebScraping():
-    op = webdriver.ChromeOptions()
-    op.add_argument('headless')
-    driver = webdriver.Chrome(executable_path ='C:\\Users\\user\\Downloads\\chromedriver.exe',options = op)
-    driver.get('https://zeenews.india.com/latest-news')
-    hs =driver.find_elements_by_xpath('//div[contains(@class ,"sec-con-box")]')
-    cs = driver.find_elements_by_xpath('//div[contains(@class ,"sec-con-box")]/p')
-    headlines = [hs[i].text for i in range(len(hs))]
-    contents = [cs[i].text for i in range(len(cs))]
-    news = []
-    for i in range(len(headlines)):
-        news.append([headlines[i], contents[i]])
+# def WebScraping():
+#     op = webdriver.ChromeOptions()
+#     op.add_argument('headless')
+#     driver = webdriver.Chrome(executable_path ='C:\\Users\\user\\Downloads\\chromedriver.exe',options = op)
+#     driver.get('https://zeenews.india.com//latest-news')
+#     hs =driver.find_elements_by_xpath('//div[contains(@class ,"sec-con-box")]')
+#     cs = driver.find_elements_by_xpath('//div[contains(@class ,"sec-con-box")]/p')
+#     headlines = [hs[i].text for i in range(len(hs))]
+#     contents = [cs[i].text for i in range(len(cs))]
+#     news = []
+#     for i in range(len(headlines)):
+#         news.append([headlines[i], contents[i]])
 
-    return news
+#     return news
 
 
-def QuotesScraping():
-    op = webdriver.ChromeOptions()
-    op.add_argument('headless')
-    driver = webdriver.Chrome(executable_path ='C:\\Users\\user\\Downloads\\chromedriver.exe',
-     options = op)
-    driver.get('https://www.inc.com/jayson-demers/51-quotes-to-inspire-success-in-your-life-and-business.html')
-    st = driver.find_elements_by_class_name('standardText')
+# def QuotesScraping():
+#     op = webdriver.ChromeOptions()
+#     op.add_argument('headless')
+#     driver = webdriver.Chrome(executable_path ='C:\\Users\\user\\Downloads\\chromedriver.exe',
+#      options = op)
+#     driver.get('https://www.inc.com/jayson-demers/51-quotes-to-inspire-success-in-your-life-and-business.html')
+#     st = driver.find_elements_by_class_name('standardText')
+#     quotes = []
+#     for i in range(len(st)):
+#         quotes.append(st[i].text)
+#     return quotes
+
+def QuotesScrapingBs():
+    
+    rs = requests.get('https://www.inc.com/jayson-demers/51-quotes-to-inspire-success-in-your-life-and-business.html')
+    html  = rs.text
+    Data = BeautifulSoup(html,'html.parser')
+    st = Data.find_all(class_ = 'standardText')
     quotes = []
     for i in range(len(st)):
         quotes.append(st[i].text)
     return quotes
 
+rs = requests.get('https://timesofindia.indiatimes.com/briefs')
+html  = rs.text
+Data = BeautifulSoup(html,'html.parser')
+toi = Data.find_all(class_ = 'brief_box')
+news = []
+for i in toi:
+    if i.h2!=None:
+        news.append([i.h2.text, i.p.text])
 
-# def WebScrapingBs():
-#     rs = requests.get('https://zeenews.india.com/latest-news')
-#     html  = rs.text
-#     Data = BeautifulSoup(html,'html.parser')
-#     zeenews =  Data.find_all(class_ = "sec-con-box")
-#     news = []
-#     for i in range(len(zeenews)):
-#       news.append([zeenews[i].a.string,zeenews[i].p.string.strip('\n\t')])
-
-#     return news
-
-
-
-
-
-
-
-
+def WebScrapingBs():
+    return news
